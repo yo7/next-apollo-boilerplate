4b388c0866b6cf8e4f5772dec1c0c8cf8718e04c
yo7 <bloom6445@gmail.com>
2019-07-26 23:57:36 +0900

    mock server

---
+++ mockserver.js
@@ -0,0 +1,16 @@
+module.exports = {
+  users: [
+    {
+      id: 1,
+      name: 'Seiya Yoshitaka',
+      posts: [
+        {
+          id: 1,
+          title: 'Railsとgraphql-rubyでGraphQLサーバーをつくる',
+          body: 'MVCなRESTからGraphQLに書き換えることでGraphQLを理解します',
+          publishedAt: new Date('2019-07-19'),
+        },
+      ],
+    },
+  ],
+}
